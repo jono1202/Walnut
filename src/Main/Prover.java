131202bd20f75f2f036b78c0a8025c7cd7f1c27b
Jonathan Yang <jonathan8589@gmail.com>
2022-08-06 20:05:33 -0400

    Fixed imports

--- src/Main/Prover.java
+++ src/Main/Prover.java
@@ -17,7 +17,14 @@
 */
 
 package Main;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
